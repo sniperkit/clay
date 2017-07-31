c3d8055fe4e5723f80d2855e9bfac4a47a53f455
qb0C80aE <qb0C80aE@gmail.com>
2017-07-31 20:59:06 +0900

    WIP

--- buildtime/build_information.go
+++ buildtime/build_information.go
@@ -4,11 +4,19 @@ import "github.com/qb0C80aE/clay/extensions"
 
 func init() {
 	var programInformation = &clayProgramInformation{
-		buildTime: "unknown",
+		buildTime: "20170731203045",
 		claySubModuleInformationList: []*claySubModuleInformation{
 			{
 				name:     "clay",
-				revision: "built-manually",
+				revision: "5b9738ee004cdec0594306a7aea14fdb57ec8eb2",
+			},
+			{
+				name:     "github.com/qb0C80aE/loam",
+				revision: "408ece1087c973979487cf47d8b8deb2671e14a1",
+			},
+			{
+				name:     "github.com/qb0C80aE/pottery",
+				revision: "7972ce0e6b854cc9faab5ab17d30fc6dfbf9b45d",
 			},
 		},
 	}
