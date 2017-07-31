59db32b01dd548e2aaa686107d10f9ce9103cabc
qb0C80aE <qb0C80aE@gmail.com>
2017-07-31 20:06:11 +0900

    WIP

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/qb0C80aE/clay/extensions"
 	_ "github.com/qb0C80aE/clay/runtime" // Import runtime package to register Clay runtime
+	_ "github.com/qb0C80aE/loam"         // Install Loam module by importing
+	_ "github.com/qb0C80aE/pottery"      // Install Pottery module by importing
 )
 
 var showVersion = flag.Bool("version", false, "show version")
