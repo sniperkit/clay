c8bab898e65c610c72cc9caca123f98d43e1a6d8
qb0C80aE <qb0C80aE@gmail.com>
2017-08-09 10:45:20 +0900

    Add demo_build.sh

---
+++ demo_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p demo
+cd demo
+cp ../linux_amd64/clay .
+cp ../examples/design.json .
+chmod 755 ./clay
+(HOST=127.0.0.1 PORT=60000 DB_MODE=file DB_FILE=./clay.db ./clay) &
+sleep 5
+curl -X PUT '127.0.0.1:60000/designs/present' -H 'Content-Type: application/json' -d @design.json
+sleep 2
+tar zcvf clay.tgz ./clay ./clay.db
+
+curl -X DELETE -H "AuthKey: ${AUTH_KEY}" https://clay-download.herokuapp.com/clay.tgz
+curl -X PUT -H "AuthKey: ${AUTH_KEY}" -H "Content-Type: multipart/form-data" https://clay-download.herokuapp.com/clay.tgz -F content=@clay.tgz -F description="Clay demo"
